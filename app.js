82bb901edbc3fc1ad134dd86a37b3beae829a61c
chipembere <chitiyobrian@gmail.com>
2020-01-14 11:28:00 +0000

    Working app and getting cities data

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-test
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 8000
+const routes = require('./routes/index')
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', routes)
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const setupFile = require('../knexfile').development;
+const knex = require('knex')({
+    client: setupFile.client,
+    connection: setupFile.connection,
+    pool: { min: 0, max: 7 }
+})
+
+router.get('/cities', (req, res) => {
+    knex.select()
+        .from('cities')
+        .then((response) => {
+            console.log(response)
+            res.send(response)
+        })
+})
+
+module.exports = router;
