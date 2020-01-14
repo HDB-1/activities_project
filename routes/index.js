705265600a76d469ff4d4041ed4a042c239f0f46
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-14 11:39:19 +0000

    add search route to query api

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex')
-
-// const setupFile = require('../knexfile').development;
-// const knex = require('knex')({
-//     client: setupFile.client,
-//     connection: setupFile.connection,
-//     pool: { min: 0, max: 7 }
-// })
+const knex = require('../db/knex');
 
 router.get('/cities', (req, res) => {
     knex.select()
-        .from('cities')
-        .then((response) => {
-            console.log(response)
-            res.send(response)
-        })
+    .from('cities')
+    .then((response) => {
+        console.log(response)
+        res.send(response)
+    })
+})
+
+router.get('/cities/:id', (req, res) => {
+
+})
+
+router.get('/citiessearch', (req, res) => {
+    knex.select()
+    .from('cities')
+    .where(req.query)
+    .on('query-response', () => {})
+    .then(function(result) {
+        res.send(result);
+        console.log(result);
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.send(error.hint)
+    })
 })
 
 module.exports = router;
